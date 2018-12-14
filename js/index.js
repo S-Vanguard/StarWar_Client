dd03e10564ed8eee540163f2e2bbbfc35090b294
SiskonEmilia <xiongyq7@mail2.sysu.edu.cn>
2018-12-14 09:59:15 +0800

    test

--- js/index.js
+++ js/index.js
@@ -11,9 +11,37 @@ let main = new Vue({
     },
     methods: {
         onSubmit: function() {
-            console.log('Sign In');
-            console.log(this.form.username)
-            console.log(this.form.password)
+            let errorMessage = this.check();
+            if (errorMessage !== "") {
+                this.$message.error(errorMessage);
+                return;
+            }
+            if (this.isSignUp === true) {
+                axios.post('/sign_up', {
+                    username: this.form.username,
+                    password: this.form.password,
+                    email: this.form.email
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    this.$message.error('连接失败：服务器无响应');
+                });
+            }
+            else {
+                axios.post('/sign_up', {
+                    username: this.form.username,
+                    password: this.form.password,
+                    email: this.form.email
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    this.$message.error('连接失败：服务器无响应');
+                });
+            }
         },
         onExchange: function() {
             this.isSignUp = !this.isSignUp;
@@ -24,6 +52,21 @@ let main = new Vue({
         toGithub: function() {
             window.location.href = 'https://github.com/ServiceComputingVanguard';
         },
+        check: function() {
+            if (this.form.username === "") {
+                return "用户名不能为空";
+            }
+            if (this.form.password === "") {
+                return "密码不能为空";
+            }
+            if (this.isSignUp && this.form.password !== this.form.confirmPsd) {
+                return "两次输入的密码不一致";
+            }
+            if (this.isSignUp && this.form.email === "") {
+                return "邮箱不能为空";
+            }
+            return "";
+        }
     },
     computed: {},
 });
